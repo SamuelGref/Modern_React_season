a796b5b646f90d10628720832fdb0b5c8d63e96b
Samuel Gref <samgref@gmail.com>
2021-04-08 14:30:17 -0400

    Determining Season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,11 +1,20 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-const App = () => {
-    );
-    return
-    <div>Hi There</div>
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter ';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
 };
 
-ReactDOM.render (<App/>, document.querySelector('#root'));
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat,new Date().getMonth)
+    console.log(season);
+    
+    return <div>{season}</div>;
+};
+
+export default SeasonDisplay;
