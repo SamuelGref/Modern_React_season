cd6271b81a5e152adaf417683fa1270ad8ec5ec9
Samuel Gref <samgref@gmail.com>
2021-04-08 13:17:48 -0400

    Seasons 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat:position.coords.latitude})
+            },
+            (err) => {
+                this.setState({errorMessage:err.message})
+            }
+          );
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.lat}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
